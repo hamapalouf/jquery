c48bf89c35bff63fc3c87a8fe619dedc1376d1a1
hamapalouf <53769555+hamapalouf@users.noreply.github.com>
2019-08-14 12:27:53 +0100

    Add files via upload

---
+++ Projects/js/editor.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+ 
+		$('.one').hide();
+		$(this).find('.one').css("opacity","0");
+ if ($('.aa').hover(function(){
+{
+
+
+
+	$('.flex').mouseover() ;
+
+
+		$('.one').show();
+		$(this).find('.one').css("opacity","1");
+
+}}));
+
+
+
+	($('.aa' ).mouseleave(function(){
+
+
+		$ (	'.one').hide();
+		$(this).find('.one').css("opacity","0");
+		
+}))
+
+
+});
